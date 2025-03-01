649efaa170f158e25e184ca34ee36b9d32f495ac
Nenad <nenadj92@gmail.com>
2025-03-01 22:31:33 +0100

    Dodata kolona kalorije

--- tablicakalorija.py
+++ tablicakalorija.py
@@ -21,13 +21,14 @@ def run_tablicakalorija():
         conn.close()
 
         # Create a Treeview widget to display data
-        tree = ttk.Treeview(tablicakalorija, columns=("Id", "Namirnica",
+        tree = ttk.Treeview(tablicakalorija, columns=("Id", "Namirnica", "Kalorije",
                                                     "Proteini", "Ugljenihidrati", "Masti"),
                                                     show="headings")
 
         # Define the columns and headings
         tree.heading("Id", text="Index")
         tree.heading("Namirnica", text="Namirnica")
+        tree.heading("Kalorije", text="Kalorije")
         tree.heading("Proteini", text="Proteini")
         tree.heading("Ugljenihidrati", text="Ugljenihidrati")
         tree.heading("Masti", text="Masti")
@@ -35,6 +36,7 @@ def run_tablicakalorija():
         # Changind column width
         tree.column("Id", width=40, anchor='center')
         tree.column("Namirnica", width=70, anchor='center')
+        tree.column("Kalorije", width=50, anchor='center')
         tree.column("Proteini", width=50, anchor='center')
         tree.column("Ugljenihidrati", width=80, anchor='center')
         tree.column("Masti", width=40, anchor='center')
@@ -49,10 +51,11 @@ def run_tablicakalorija():
     def adding_values_in_database():
         conn = sqlite3.connect("tablicakalorija.db")
         cursor = conn.cursor()
-        query = '''INSERT INTO tablicakalorija (namirnica, proteini, ugljenihidrati, masti)
-                    VALUES (?, ?, ?, ?)'''
+        query = '''INSERT INTO tablicakalorija (namirnica, kalorije, proteini, ugljenihidrati, masti)
+                    VALUES (?, ?, ?, ?, ?)'''
         cursor.execute(query,
                     (namirnica_entry.get(),
+                    float(kalorije_entry.get()),
                     float(proteini_entry.get()),
                     float(ugljenihidrati_entry.get()),
                     float(masti_entry.get())))
@@ -77,13 +80,14 @@ def run_tablicakalorija():
         cursor.execute('''SELECT * FROM tablicakalorija''')
         data = cursor.fetchall()
         # Create a Treeview widget to display data
-        tree = ttk.Treeview(tablicakalorija, columns=("Id", "Namirnica",
+        tree = ttk.Treeview(tablicakalorija, columns=("Id", "Namirnica", "Kalorije",
                                                     "Proteini", "Ugljenihidrati", "Masti"),
                                                     show="headings")
 
         # Define the columns and headings
         tree.heading("Id", text="Index")
         tree.heading("Namirnica", text="Namirnica")
+        tree.heading("Kalorije", text="Kalorije")
         tree.heading("Proteini", text="Proteini")
         tree.heading("Ugljenihidrati", text="Ugljenihidrati")
         tree.heading("Masti", text="Masti")
@@ -91,6 +95,7 @@ def run_tablicakalorija():
         # Changind column width
         tree.column("Id", width=40, anchor='center')
         tree.column("Namirnica", width=70, anchor='center')
+        tree.column("Kalorije", width=50, anchor='center')
         tree.column("Proteini", width=50, anchor='center')
         tree.column("Ugljenihidrati", width=80, anchor='center')
         tree.column("Masti", width=40, anchor='center')
@@ -99,16 +104,18 @@ def run_tablicakalorija():
         for row in data:
             tree.insert("", "end", values=row)
 
-        tree.grid(row=5, column=0, columnspan=2)
+        tree.grid(row=6, column=0, columnspan=2)
 
         # Labele za namirnicu i makronutrijente
         namirnica_label = Label(tablicakalorija, text="Namirnica:")
+        kalorije_label = Label(tablicakalorija, text="Kalorije (kcal):")
         proteini_label = Label(tablicakalorija, text="Proteini (g):")
         ugljenihidrati_label = Label(tablicakalorija, text="Ugljeni hidrati (g):")
         masti_label = Label(tablicakalorija, text="Masti (g):")
 
         # Entry polja za namirnicu i makronutrijente
         namirnica_entry = Entry(tablicakalorija)
+        kalorije_entry = Entry(tablicakalorija)
         proteini_entry = Entry(tablicakalorija)
         ugljenihidrati_entry = Entry(tablicakalorija)
         masti_entry = Entry(tablicakalorija)
@@ -116,32 +123,61 @@ def run_tablicakalorija():
 
         # Pozicioniranje labela za namirnicu i makronutrijente
         namirnica_label.grid(row=0, column=0, padx=5, sticky='w')
-        proteini_label.grid(row=1, column=0, padx=5, sticky='w')
-        ugljenihidrati_label.grid(row=2, column=0, padx=5, sticky='w')
-        masti_label.grid(row=3, column=0, padx= 5, sticky='w')
+        kalorije_label.grid(row=1, column=0, padx=5, sticky='w')
+        proteini_label.grid(row=2, column=0, padx=5, sticky='w')
+        ugljenihidrati_label.grid(row=3, column=0, padx=5, sticky='w')
+        masti_label.grid(row=4, column=0, padx= 5, sticky='w')
 
         # Pozicioniranje entry polja namirnicu i makronutrijente
         namirnica_entry.grid(row=0, column=1, padx=5)
-        proteini_entry.grid(row=1, column=1, padx=5)
-        ugljenihidrati_entry.grid(row=2, column=1, padx=5)
-        masti_entry.grid(row=3, column=1, padx=5)
+        kalorije_entry.grid(row=1, column=1, padx=5)
+        proteini_entry.grid(row=2, column=1, padx=5)
+        ugljenihidrati_entry.grid(row=3, column=1, padx=5)
+        masti_entry.grid(row=4, column=1, padx=5)
 
-        ok_button = Button(tablicakalorija, text="OK")
-        ok_button.grid(row=4, column=0, padx=5, pady=10, sticky='w')
-        odustani_button = Button(tablicakalorija, text="Odustani")
-        odustani_button.grid(row=4, column=0, sticky='e')
+        def change_values_in_database_ok():
+            conn = sqlite3.connect("tablicakalorija.db")
+            cursor = conn.cursor()
+            query = '''UPDATE tablicakalorija SET namirnica = ?, kalorije = ?, proteini = ?, ugljenihidrati = ?, masti = ? WHERE id = ?'''
+            cursor.execute(query,
+                        (namirnica_entry.get(),
+                        float(kalorije_entry.get()),
+                        float(proteini_entry.get()),
+                        float(ugljenihidrati_entry.get()),
+                        float(masti_entry.get()),
+                        tree.item(tree.focus())['values'][0]))
+            conn.commit()
+            cursor.close()
+            conn.close()
+            messagebox.showinfo("Information", "Vrednost uspesno izmenjena!")
+            namirnica_entry.delete(0, 'end')
+            proteini_entry.delete(0, 'end')
+            ugljenihidrati_entry.delete(0, 'end')
+            masti_entry.delete(0, 'end')
+            prikazVrednosti()
+
+        def change_values_in_database_cancel():
+            tablicakalorija.destroy()
+
+
+        ok_button = Button(tablicakalorija, text="OK", command=change_values_in_database_ok)
+        ok_button.grid(row=5, column=0, padx=5, pady=10, sticky='w')
+        odustani_button = Button(tablicakalorija, text="Odustani", command=change_values_in_database_cancel)
+        odustani_button.grid(row=5, column=0, sticky='e')
 
         def on_row_selected(event):
             selected_item = tree.focus()
             item_values = tree.item(selected_item, 'values')
             namirnica_entry.delete(0, 'end')
             namirnica_entry.insert(0, item_values[1])
+            kalorije_entry.delete(0, 'end')
+            kalorije_entry.insert(0, item_values[2])
             proteini_entry.delete(0, 'end')
-            proteini_entry.insert(0, item_values[2])
+            proteini_entry.insert(0, item_values[3])
             ugljenihidrati_entry.delete(0, 'end')
-            ugljenihidrati_entry.insert(0, item_values[3])
+            ugljenihidrati_entry.insert(0, item_values[4])
             masti_entry.delete(0, 'end')
-            masti_entry.insert(0, item_values[4])
+            masti_entry.insert(0, item_values[5])
 
         tree.bind('<ButtonRelease-1>', on_row_selected)
 
